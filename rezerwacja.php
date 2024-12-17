cb011467bae50e3029d6f73fc1e18515eafe3746
Deelma <156914350+Deelma@users.noreply.github.com>
2024-12-17 09:30:44 +0100

    oops

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,28 +1,33 @@
 <?php
 
-// Połączenie z serwerem bazodanowym
+echo "Dodano rezasdasdy";
+
+
 $conn = mysqli_connect('localhost', 'root', '', 'restauracja_4ti_gr1');
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if($conn->connect_error){
+    die("Connection failed: " 
+        . $conn->connect_error);
 }
 
-// Pobieranie danych z formularza
 $data = $_GET['data'];
+
 $ilosc_osob = $_GET['ilosc_osob'];
+
 $nr_telefonu = $_GET['nr_telefonu'];
+
 $przetwarzanie = $_GET['przetwarzanie'];
 
-// Zapytanie wstawiające rekord do tabeli rezerwacje
-$sql = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$ilosc_osob', '$nr_telefonu')";
 
-// Wykonanie zapytania i sprawdzenie, czy zostało wykonane poprawnie
-if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO `rezerwacje` (id, data_rez, liczba_osob, telefon) VALUES ($data, $ilosc_osob, $nr_telefonu, $przetwarzanie)";
+
+
+if($conn->query($sql) === TRUE){
     echo "Dodano rezerwację do bazy";
-} else {
+}else{
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Zamknięcie połączenia z serwerem
 $conn->close();
+
 ?>
