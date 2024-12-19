e76dd3a76d3897cf3798f771157aabd6d7380e7a
Deelma <156914350+Deelma@users.noreply.github.com>
2024-12-19 14:34:23 +0100

    Add files via upload

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,33 +1,33 @@
 <?php
 
-echo "Dodano rezasdasdy";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $conn = mysqli_connect('localhost', 'root', '', 'restauracja_4ti_gr1');
+
+    if($conn->connect_error){
+        die("Connection failed: " 
+            . $conn->connect_error);
+    }
+
+    $data = $_GET['data'];
+
+    $ilosc_osob = $_GET['ilosc_osob'];
+
+    $nr_telefonu = $_GET['nr_telefonu'];
+
+    $przetwarzanie = $_GET['przetwarzanie'];
 
 
-$conn = mysqli_connect('localhost', 'root', '', 'restauracja_4ti_gr1');
+    $sql = "INSERT INTO rezerwacje (id, nr_stolika, data_rez, liczba_osob, telefon) VALUES (null, null, $data, $ilosc_osob, $nr_telefonu)";
 
-if($conn->connect_error){
-    die("Connection failed: " 
-        . $conn->connect_error);
+
+    if($conn->query($sql) === TRUE){
+        echo "Dodano rezerwację do bazy";
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 }
-
-$data = $_GET['data'];
-
-$ilosc_osob = $_GET['ilosc_osob'];
-
-$nr_telefonu = $_GET['nr_telefonu'];
-
-$przetwarzanie = $_GET['przetwarzanie'];
-
-
-$sql = "INSERT INTO `rezerwacje` (id, data_rez, liczba_osob, telefon) VALUES ($data, $ilosc_osob, $nr_telefonu, $przetwarzanie)";
-
-
-if($conn->query($sql) === TRUE){
-    echo "Dodano rezerwację do bazy";
-}else{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 
 ?>
